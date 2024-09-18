67cc2f886d5ddd9ff85910b7da71c5ab4cad5de0
KenJohn28 <johnsonmok1129@gmail.com>
2024-09-17 20:53:37 -0400

    Added the GitHub repo url

--- src/App.js
+++ src/App.js
@@ -2,7 +2,24 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="/" target="_blank">
+          Kenya Johnson
+        </a>{" "}
+        and is {" "}
+        <a
+          href="https://github.com/KenJohn28/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
