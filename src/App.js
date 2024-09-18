3673d688ea591fdd4861aa1b0dec2367d7c73ae4
KenJohn28 <johnsonmok1129@gmail.com>
2024-09-18 19:34:02 -0400

    Added bootstrap to my project

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="/" target="_blank">
-          Kenya Johnson
-        </a>{" "}
-        and is {" "}
-        <a
-          href="https://github.com/KenJohn28/react-weather-app"
-          target="_blank"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="/" target="_blank">
+            Kenya Johnson
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/KenJohn28/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
