22546b4fb4274b8878fe8a2f02aeff3bb25de442
KenJohn28 <johnsonmok1129@gmail.com>
2024-09-23 20:21:21 -0400

    Created a new component to convert temperature based on unit

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -14,9 +14,7 @@ export default function FormattedDate(props) {
   let hours = props.date.getHours();
   if (hours< 10) {hours = `0${hours}`}
   let minutes = props.date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${mintues}`;
-  }
+  if (minutes < 10) {minutes = `0${minutes}`;}
 
   return (
     <div>
