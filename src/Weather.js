55cbad5e135d05262525e2dd42deaa9ece407414
KenJohn28 <johnsonmok1129@gmail.com>
2024-09-27 08:38:02 -0400

    Created the basic structure of the daily weather forecast

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
 import "./Weather.css";
 
@@ -64,6 +64,7 @@ export default function Weather(props) {
         </form>
 
         <WeatherInfo data={weatherData} />
+        <WeatherForecast />
       </div>
     );
   } else {
