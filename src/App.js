19ee36b1c2cf6b5a2e60967e71b5cb053cbcca6c
KenJohn28 <johnsonmok1129@gmail.com>
2024-09-30 09:13:20 -0400

    Minor styling changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <h1 className="text-center">Weather App</h1>
         <Weather defaultCity="New York" />
         <footer>
           This project was coded by{" "}
